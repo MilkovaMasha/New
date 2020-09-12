62184032d34a98e60b8902fb3e627845b5093b11
MilkovaMasha <61597532+MilkovaMasha@users.noreply.github.com>
2020-09-12 18:01:51 +0300

    Create lw3.php

---
+++ lw3/lw3.php
@@ -0,0 +1,12 @@
+<?php
+	function getGETParameter(string $text): ?string
+	{
+		return isset($_GET[$text]) ? (string) $_GET[$text] : null;
+	}
+	$text = getGETParameter('text');
+    while (preg_match("/  /", $text))
+	{
+		$text = str_replace('  ', ' ', $text);
+	}
+	echo trim($text);
+?>
